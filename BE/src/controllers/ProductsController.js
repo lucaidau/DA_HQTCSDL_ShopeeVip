d52e8ecd2bf57367613eb8e955c4a2c903b8e518
Lucaidau <dotoan270806@gmail.com>
2026-03-19 21:17:22 +0700

    Finish product detail logic in product controller

--- BE/src/controllers/ProductsController.js
+++ BE/src/controllers/ProductsController.js
@@ -1,6 +1,7 @@
 const { sql, poolPromise } = require("../config/connect");
 
 class ProductsController {
+  //[GET] /sanpham
   async trangChu(req, res) {
     try {
       const page = parseInt(req.query.page) || 1;
@@ -11,13 +12,11 @@ class ProductsController {
         .input("PageSize", sql.Int, 20)
         .execute("sp_LaySanPham");
 
-      return res
-        .status(200)
-        .json({
-          message: "Lấy sản phẩm thành công",
-          trangHienTai: page,
-          danhSachSanPham: result.recordsets,
-        });
+      return res.status(200).json({
+        message: "Lấy sản phẩm thành công",
+        trangHienTai: page,
+        danhSachSanPham: result.recordsets,
+      });
     } catch (err) {
       console.log("Err: ", err);
       return res
@@ -26,13 +25,27 @@ class ProductsController {
     }
   }
 
+  //[GET] /sanpham/:id
   async sanPham(req, res) {
     try {
-      const id = parent(req.params.id);
+      const id = parseInt(req.params.id);
 
       const pool = await poolPromise;
-      const result = await pool.request().input('IDSanPham', sql.Int, id);
-    } catch (error) {}
+
+      const result = await pool
+        .request()
+        .input("IDSanPham", sql.Int, id)
+        .execute("sp_LayChiTietSanPham");
+
+      // if (result.recordset.length === 0) {
+      //   return res.status(404).json({ message: "Không tìm thấy sản phẩm" });
+      // }
+
+      return res.status(200).json({ san_pham: result.recordset[0] });
+    } catch (error) {
+      console.log("Err: ", error);
+      return res.status(500).json({ message: "Lỗi Server!" });
+    }
   }
 }
 
