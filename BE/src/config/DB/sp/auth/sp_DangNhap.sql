8575f383ecc79103ebf3bb54e17694b1b1174de3
Pham Tien <phamquoctien1142@gmail.com>
2026-03-22 06:12:18 +0700

    Add Role Play in sp_DangNhap

--- BE/src/config/DB/sp/auth/sp_DangNhap.sql
+++ BE/src/config/DB/sp/auth/sp_DangNhap.sql
@@ -8,13 +8,25 @@ AS
 
         IF EXISTS (SELECT * FROM TAI_KHOAN WHERE @TenDangNhap = TenDangNhap AND @MatKhau = MatKhau)
         BEGIN
-            SELECT *
-            FROM TAI_KHOAN
-            WHERE TenDangNhap = @TenDangNhap
+            SELECT 
+            tk.IDTaiKhoan,
+            tk.Ten,
+            nm.IDNguoiMua,
+            s.IDShop,
+			CASE
+                WHEN s.IDShop IS NOT NULL THEN 'SHOP'
+                WHEN nm.IDNguoiMua IS NOT NULL THEN 'BUYER'
+            END AS 'VaiTro'
+             FROM TAI_KHOAN tk
+			 LEFT JOIN NGUOI_MUA nm ON tk.IDTaiKhoan = nm.IDTaiKhoan
+			 LEFT JOIN SHOP s ON s.IDTaiKhoan = tk.IDTaiKhoan
+            WHERE tk.TenDangNhap = @TenDangNhap AND MatKhau = @MatKhau
         END
 
         ELSE
         BEGIN
             RAISERROR(N'Tên đăng nhập hoặc mật khẩu không đúng', 16, 1)
         END
+
+        
     END
