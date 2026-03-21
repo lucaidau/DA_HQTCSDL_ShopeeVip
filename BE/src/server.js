6fe1a3790ebe6a20a2548283f0d68aee9ea166c0
Pham Tien <phamquoctien1142@gmail.com>
2026-03-21 21:42:54 +0700

    Config CORS

--- BE/src/server.js
+++ BE/src/server.js
@@ -4,7 +4,9 @@ const port = 3000;
 const cors = require("cors")
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: "https://sv-da-hqtcsdl-shopee-vip.vercel.app/"
+}))
 
 const route = require("./routers/index");
 route(app);
