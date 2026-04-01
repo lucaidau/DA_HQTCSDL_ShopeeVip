9153fd2f176425acff7ff7374cf0724d543e1e1c
Lucaidau <dotoan270806@gmail.com>
2026-04-01 14:19:50 +0700

    Create shop router

---
+++ BE/src/controllers/ShopController.js
@@ -0,0 +1,26 @@
+const { sql, poolPromise } = require("../config/connect");
+
+class ShopController {
+  // [GET] Shop/:id
+  async sanPham(req, res) {
+    try {
+      const shopID = parseInt(req.params.id);
+
+      const pool = await poolPromise;
+      const result = await pool
+        .request()
+        .input("IDShop", sql.Int, shopID)
+        .execute("sp_LaySanPhamShop");
+
+      return res.status(200).json({
+        message: "Lấy sản phẩm shop thành công",
+        shopProduct: result.recordset,
+      });
+    } catch (error) {
+      console.log("Err: ", error);
+      return res.status(500).json({ message: "Lỗi server!!" });
+    }
+  }
+}
+
+module.exports = new ShopController();
