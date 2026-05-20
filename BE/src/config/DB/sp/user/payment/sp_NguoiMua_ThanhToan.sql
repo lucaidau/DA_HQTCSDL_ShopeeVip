52bbe4ad80c8afb85bcc27f2880d85232687be0b
lucaidau <dotoan270806@gmail.com>
2026-05-21 01:57:59 +0700

    Done Cart Logic and API

--- BE/src/config/DB/sp/user/payment/sp_NguoiMua_ThanhToan.sql
+++ BE/src/config/DB/sp/user/payment/sp_NguoiMua_ThanhToan.sql
@@ -1,6 +1,34 @@
 CREATE PROC sp_NguoiMua_ThanhToan
-
+@IDNguoiMua INT,
+@IDBanSao INT,
+@SoLuongMua INT,
+@DiaChi NVARCHAR(255),
+@TongTien DECIMAL(18,2)
 AS
 BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        DECLARE @TonKho INT;
+        SELECT @TonKho = SoLuongTonKho
+        FROM BAN_SAO_SAN_PHAM WITH (UPDLOCK)
+        WHERE @IDBanSao = IDBanSao;
 
-END
+        IF(@TonKho < @SoLuongMua)
+        BEGIN
+            RAISERROR(N'',16,1);
+        END
+
+        UPDATE BAN_SAO_SAN_PHAM
+        SET SoLuongTonKho = SoLuongTonKho - @SoLuongMua
+        WHERE IDBanSao = @IDBanSao;
+            
+        DECLARE @NewIDDonHang INT;
+
+    END TRY
+
+    BEGIN CATCH
+        IF(@@TRANCOUNT > 0)
+            ROLLBACK TRANSACTION;
+        SELECT 0 AS Success, ERROR_MESSAGE() AS Message;
+    END CATCH
+END;
